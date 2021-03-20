bb7f226d98cf1825e6972ad1942a77f548da0113
Chaitanya JVNM <chaitanyajvnm@gmail.com>
2021-03-21 02:10:02 +0530

    Functionall

--- tracker.py
+++ tracker.py
@@ -1,12 +1,6 @@
-#works only for flipkart now
-
-#TODO : figure out amazon api and use it for amazon urls
-
-#%%
 import requests
 from glob import glob
 from bs4 import BeautifulSoup
-# import pandas
 from datetime import datetime
 from time import sleep
 import json
@@ -20,60 +14,48 @@ from telegram.ext import (
     ConversationHandler,
     CallbackContext,
 )
-import requests
 import re
 import os
 from os import environ
 
-#%%
 HEADERS = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
 
-#%%
 #enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
-#%%
 logger = logging.getLogger(__name__)
 
-# GENDER, PHOTO, LOCATION, BIO = range(4)
-prod = range(1)
-
-# def echo(update: Update, context: CallbackContext) -> None:
-#     """Echo the user message."""
-#     update.message.reply_text(update.message.text)
-
-#%%
-def start(update: Update, context: CallbackContext):
-    logger.info("User %s started the conversation.", user.first_name)
+def start(update: Update, context: CallbackContext) -> None:
+    logger.info("User %s started the conversation.", update.message.from_user.username)
     update.message.reply_text(
-        "Hi! I'm ezTracker. I can track prices of your products and let you know if they dip.(Currently only flip[kart supported.)"
-        "Type /url and enter an url to start tracking it"
-        'You can always send /help to get the full list of commands at your disposal'
-        'Send /cancel to stop tracking.\n\n'
-        # reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+        "Hi! I'm ezTracker. I can track prices of your products and let you know if they dip.(Currently only flipkart supported.)\n"
+        "Type /url and enter an url to start tracking it\n"
+        'You can always send /help to get the full list of commands at your disposal\n'
+        'Send /cancel to stop tracking.\n'
     )
 
-#%%
 def scrapeURL(update: Update, context: CallbackContext) -> None:
+    urlRegex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = update.message.text
+    if not(re.match(urlRegex,url)):
+        update.message.reply_text("Please enter a valid URL")
+        return
 
     page = requests.get(url,headers=HEADERS)
     soup = BeautifulSoup(page.content, features="lxml")
 
     #Product Name section
     try:
-        #pName = soup.find(id='productTitle').get_text().strip() --Amazon
         pName = soup.find_all(class_="B_NuCI")[0].get_text().replace(u'\xa0', u' ').strip()
     except:
         pName = ''
 
     # Product price section
     try:
-        # pPrice = float(soup.find(id='priceblock_ourprice').get_text().replace('.', '').replace('₹', '').replace(',', '.').strip()) --Amazon
         pPrice = soup.find_all(class_="_30jeq3")[0].get_text().strip()
     except:
         pPrice = ''
@@ -84,77 +66,39 @@ def scrapeURL(update: Update, context: CallbackContext) -> None:
     except:
         pRating = ''
 
-    #try:
-        # soup.select('#availability .a-color-state')[0].get_text().strip() --Amazon availability
-        # stock = 'Out of Stock'
-    # except:
-    #     stock = 'Available'
+    update.message.reply_text( "[ Rating: " + pRating + " ]\n" + pName + "\n[ Price: " + pPrice + " ]")
 
-    #print( "[ Rating: " + pRating + " ]\n" + pName + "\n[ Price: " + pPrice + " ]")
-    # print ('{ "name": "' + pName + '", "rating":"' + pRating + '", "price":"' + pPrice + '" }')
-    return '{ "name": "' + pName + '", "rating":"' + pRating + '", "price":"' + pPrice + '" }'
-    update.message.reply_text( "[ Rating: " + product['rating'] + " ]\n" + product['name'] + "\n[ Price: " + product['price'] + " ]")
+    return
 
-#%%
-# def updateChat(update: Update, context: CallbackContext):
-#     #get the recipients chat id
-#     chat_id = update.message.chat_id
-#     # bot.send_photo(chat_id=chat_id, photo=url)
-#     bot.send_message(chat_id=chat_id, text=scrapeURL)
-
-#%%
-def cancel(update: Update, context: CallbackContext) -> int:
+def cancel(update: Update, context: CallbackContext) -> None:
     user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
+    logger.info("User %s canceled the conversation.", update.message.from_user.username)
     update.message.reply_text(
         'Bye! I hope we can cross paths again some day.', reply_markup=ReplyKeyboardRemove()
     )
 
     return ConversationHandler.END
 
-#%%
-def main():
-    updater = Updater('1590128551:AAFZO4q13bx_s4K8cZ3zr9io3HZasw1_vuY')
-    # updater = Updater(environ['TBOT-KEY'])
-    dispatcher = updater.dispatcher
-    urlRegex = 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)'
-    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-        states={
-            prod: [MessageHandler(Filters.regex(urlRegex), scrapeURL)]
-            # GENDER: [MessageHandler(Filters.regex('^(Boy|Girl|Other)$'), gender)],
-            # PHOTO: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
-            # LOCATION: [
-            #     MessageHandler(Filters.location, location),
-            #     CommandHandler('skip', skip_location),
-            # ],
-            # BIO: [MessageHandler(Filters.text & ~Filters.command, bio)],
-        },
-        fallbacks=[CommandHandler('cancel', cancel)],
+def help_command(update: Update, context: CallbackContext) -> None:
+    logger.info("User %s used the help command.", update.message.from_user.username)
+    update.message.reply_text(
+        "Hi! I'm ezTracker. I can track prices of your products and let you know if they dip.(Currently only flipkart supported.)\n"
+        "Type /url and enter an url to start tracking it\n"
+        'You can always send /help to get the full list of commands at your disposal\n'
+        'Send /cancel to stop tracking.\n'
     )
 
-    dispatcher.add_handler(conv_handler)
-    # dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, scrapeURL))
+def main():
+    updater = Updater(environ['TBOT_KEY'])
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("cancel", cancel))
+    dispatcher.add_handler(CommandHandler("help", help_command))
 
-    # dp.add_handler(CommandHandler('bop',updateChat))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, scrapeURL))
+
     updater.start_polling()
     updater.idle()
 
-#%%
 if __name__ == '__main__':
     main()
-
-
-
-
-#%%
-# item = "https://www.amazon.in/Logitech-Lightspeed-Suspension-LIGHTSYNC-Technology/dp/B08HNCG8WQ/ref=sr_1_10_sspa?crid=3QA8S7TAU8UXS&dchild=1"
-link = str('https://www.flipkart.com/ambrane-aqc-56-18-w-3-mobile-charger-detachable-cable/p/itm053fe47c4f26c?pid=ACCFNACQPFAQBHTE&lid=LSTACCFNACQPFAQBHTE7AIBOQ&marketplace=FLIPKART&srno=b_1_5&otracker=hp_omu_Deals%2Bof%2Bthe%2BDay_4_3.dealCard.OMU_NW53DUEZOI54_3&otracker1=hp_omu_SECTIONED_neo%2Fmerchandising_Deals%2Bof%2Bthe%2BDay_NA_dealCard_cc_4_NA_view-all_3&fm=neo%2Fmerchandising&iid=acf14168-0de3-4f65-af60-7cc22381d7d2.ACCFNACQPFAQBHTE.SEARCH&ppt=browse&ppn=browse&ssid=4dkfw756u34qbsow1613827233912')
-
-#%%
-product = json.loads(main(link))
-print ( "[ Rating: " + product['rating'] + " ]\n" + product['name'] + "\n[ Price: " + product['price'] + " ]")
-    
-# %%
-#testing block
